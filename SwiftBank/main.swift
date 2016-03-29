0bef2845b70c70e5fc63c6e47bb3086be4d749fd
afjoseph <abdullahfjoseph@gmail.com>
2016-03-29 11:14:12 +0800

    Initial Commit

---
+++ SwiftBank/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftBank
+//
+//  Created by Mindvalley on 3/29/16.
+//  Copyright © 2016 Mindvalley. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
